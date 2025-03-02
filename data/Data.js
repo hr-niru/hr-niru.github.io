485ac05d4a59e5e21a22bdd64789e72c63a255d2
hr-niru <info.njs20794@gmail.com>
2025-03-02 15:32:47 +0530

    Update Data.js

--- data/Data.js
+++ data/Data.js
@@ -16,31 +16,3 @@ export const skills = [
   { name: "HR Policies & Process Improvement", progress: "90" },
   { name: "Onboarding & Training7", progress: "85" },
 ];
-
-export const projectData = [
-  {
-    image: "assets/img/portfolio/Texture.png",
-    url: "https://primecarers.co.uk/",
-    alt: "Texture",
-    details:
-      "Revolutionize your digital strategy with our SAAS project, combining REST APIs and Django templates for effortless text analysis and ad enhancements. Seamlessly integrate API functionalities into your workflow, leveraging Django templates for dynamic content optimization. Elevate advertising precision and streamline text analysis for unparalleled marketing insights. Stay ahead in the competitive landscape with our user-friendly and robust solution",
-  },
-  {
-    image: "assets/img/portfolio/Sharelyze.png",
-    url: "https://sharelyze.com/",
-    alt: "ShareLyze",
-    details: "Details about ShareLyze: This project is focused on...",
-  },
-  {
-    image: "assets/img/portfolio/portfolio-15.png",
-    url: "https://www.feltiv.com/",
-    alt: "Feltiv",
-    details: "Details about Feltiv: The main objective of this project...",
-  },
-  {
-    image: "assets/img/portfolio/portfolio-14.png",
-    url: "https://www.feltiv.com/",
-    alt: "Feltiv",
-    details: "Details about Feltiv: This project involves...",
-  },
-];
